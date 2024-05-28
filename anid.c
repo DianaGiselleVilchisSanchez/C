8d9e9e102b3dce2120a36ff297012e09ac863754
Diana Vichis <165976607+DianaGiselleVilchisSanchez@users.noreply.github.com>
2024-05-27 23:48:29 -0600

    use of strlen

---
+++ anid.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <string.h>
+int main (){
+    int len;
+    char cad []="Es una cadena";
+    len = strlen(cad);
+    printf("la longitud es de:\'%s\'es : %d\n", cad, len);
+    return 0;
+}
