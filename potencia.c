d3f1e75e7a4f9521d187699461d05bc210e9a997
Diana Vichis <165976607+DianaGiselleVilchisSanchez@users.noreply.github.com>
2024-06-13 10:17:01 -0600

    functions

---
+++ potencia.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main (void){
+    int x, y, max;
+    x=2;
+    y=3;
+    max = potencia (x,y);
+    printf ("la potencia es %d", max);
+    return 0;
+}
+int potencia (int a, int b){
+    if (b < 1)
+        return 1;
+        return a * potencia (a, b-1);
+}
