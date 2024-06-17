71574027099c1635bda2d3657a7f2c192a988ca3
Diana Vichis <165976607+DianaGiselleVilchisSanchez@users.noreply.github.com>
2024-06-17 14:29:45 -0600

    struct

--- tarea3.c
+++ tarea3.c
@@ -1,31 +1,31 @@
 #include <stdio.h>
-#include <string.h>
-int main() {
-    char palabra1[100];
-    char palabra2[100];
 
-    printf("Ingrese la primera palabra: ");
-    scanf("%s", palabra1);
-    printf("Ingrese la segunda palabra: ");
-    scanf("%s", palabra2);
+struct empleado {
+    char nombre[100];
+    char sexo[30];
+    int sueldo;
+};
 
-    int longitud1 = strlen(palabra1);
-    int longitud2 = strlen(palabra2);
+int main(void) {
+    struct empleado empleado;
+    char nombre [100];
+    char sexo[30];
+    printf ("DATACIC \n");
+   
+    printf("Ingrese el nombre: ");
+    scanf("%s", empleado.nombre);
 
-    if (longitud1 == longitud2) {
-        int comparacion = strcmp(palabra1, palabra2);
-        if (comparacion == 0) {
-            printf("¡Ambas palabras son iguales!\n");
-        } else if (comparacion < 0) {
-            printf("La primera palabra '%s' es menor que la segunda palabra '%s'\n", palabra1, palabra2);
-        } else {
-            printf("La primera palabra '%s' es mayor que la segunda palabra '%s'\n", palabra1, palabra2);
-        }
-    } else if (longitud1 < longitud2) {
-        printf("La primera palabra '%s' es menor que la segunda palabra '%s'\n", palabra1, palabra2);
-    } else {
-        printf("La primera palabra '%s' es mayor que la segunda palabra '%s'\n", palabra1, palabra2);
-    }
+    printf("Ingrese el sexo: ");
+    scanf("%s", empleado.sexo);
+
+    printf("Ingrese el sueldo: ");
+    scanf("%d", &empleado.sueldo);
+
+
+    printf("\nInformacion del empleado:\n");
+    printf("Nombre: %s\n", empleado.nombre);
+    printf("Sexo: %s\n", empleado.sexo);
+    printf("Sueldo: %d\n", empleado.sueldo);
 
     return 0;
 }
