16da8185428d9a02c37fe8af435cd54ff5b44fed
Diana Vichis <165976607+DianaGiselleVilchisSanchez@users.noreply.github.com>
2024-06-23 17:26:52 -0600

    ciclos de reloj

---
+++ ciclos.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int x;
+    int * arreglo;
+    printf("Ingrese el tamaño del arreglo: ");
+    scanf("%d", &x);
+      arreglo = (int *)malloc (x * sizeof(int));
+    
+    if(arreglo == NULL)  (exit (1));
+    for (int i = 0; i < x; i++) {
+        printf("Ingrese el valor numero %d:\n", i+1);
+        scanf("%d", &arreglo[i]);
+    }
+    printf("El arreglo es:");
+    for (int i=0; i<x; i++) {
+        printf("%d ", arreglo[i]);
+        printf ("\n");
+    }
+    
+    free(arreglo);
+    return 0;
+}
